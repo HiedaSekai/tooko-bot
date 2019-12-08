25f61696b2837de63ccf555ee44aeca907a436c1
風間わたる <i@wataru.moe>
2019-12-08 08:15:25 +0800

    _(:з」∠)_

--- src/main/java/tooko/twitter/nsfw/NSFWTest.java
+++ src/main/java/tooko/twitter/nsfw/NSFWTest.java
@@ -6,6 +6,7 @@ import twitter4j.*;
 import tooko.main.*;
 import tooko.twitter.archives.*;
 import tooko.twitter.nsfw.NSFWDetector.*;
+import tooko.td.TdApi.*;
 
 public class NSFWTest extends TwitterHandler {
 
@@ -18,34 +19,52 @@ public class NSFWTest extends TwitterHandler {
 
 
     @Override
-    public void onFunction(TdApi.User user, long chatId, TdApi.Message message, String function, String param, String[] params, String[] originParams, TwitterAccount account) {
+    public void onFunction(TdApi.User user, final long chatId, TdApi.Message message, String function, final String param, String[] params, String[] originParams, final TwitterAccount account) {
 
-        try {
-            
-            NSFWDetector.NSRC[] rcs = NSFWDetector.predetectStatus(account.mkApi().getUserTimeline(Fn.parseScreenName(param), new Paging().count(200)));
+        client.asyncPool.execute(new Runnable() {
 
-            int drawings = 0,hentai = 0,porn = 0,sexy = 0;
-            
-            for (NSFWDetector.NSRC rc : rcs) {
-                
-                switch (rc) {
-                    
-                  case DRAWINGS: drawings ++;break;
-                  case HENTAI: hentai ++; break;
-                  case PORN: porn ++;break;
-                  case SEXY: sexy ++;break;
-                  
+                @Override
+                public void run() {
+
+                    try {
+
+                        TdApi.Message stat = sync(Fn.sendText(chatId, Fn.plainText("fetching statuses...")));
+
+                        ResponseList<Status> statuses =  account.mkApi().getUserTimeline(Fn.parseScreenName(param), new Paging().count(200));
+
+                        int drawings = 0,hentai = 0,porn = 0,sexy = 0;
+
+                        for (int index = 0;index > statuses.size();index ++) {
+
+                            Status status = statuses.get(index);
+                            
+                            if (status.getMediaEntities().length == 0) continue;
+                          
+                            NSFWDetector.NSRC rc = NSFWDetector.predetectStatus(status)[0];
+
+                            switch (rc) {
+
+                                case DRAWINGS: drawings ++;break;
+                                case HENTAI: hentai ++; break;
+                                case PORN: porn ++;break;
+                                case SEXY: sexy ++;break;
+
+                            }
+
+                            send(Fn.editText(stat, Fn.plainText("predicting... {} / {},\nDRAWINGS: {}\nHENTAI: {}\nPORN: {}\nSEXY: {}",index + 1,statuses.size(), drawings, hentai, porn, sexy)));
+
+                        }
+
+                    } catch (Exception e) {
+
+                        send(Fn.sendText(chatId, Fn.plainText(Fn.parseError(e))));
+
+                    }
+
+
                 }
-                
-            }
-            
-            send(Fn.sendText(chatId, Fn.plainText("DRAWINGS: {}\nHENTAI: {}\nPORN: {}\nSEXY: {}",drawings,hentai,porn,sexy)));
+            });
 
-        } catch (Exception e) {
-
-            send(Fn.sendText(chatId, Fn.plainText(Fn.parseError(e))));
-
-        }
 
     }
 
