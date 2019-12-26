3d8320314fc8002c3d3d328d726dac49751f3157
風間わたる <i@wataru.moe>
2019-12-27 01:31:45 +0800

    _(:з」∠)_

--- src/main/java/tookox/core/client/TdBotHandler.kt
+++ src/main/java/tookox/core/client/TdBotHandler.kt
@@ -9,6 +9,7 @@ open class TdBotHandler : TdHandler(), TdBotAbsHandler {
 
     override fun onLoginFailed(ex: TdException) = Unit
     override fun onUndefinedFunction(userId: Int, chatId: Long, message: TdApi.Message, function: String, param: String, params: Array<String>, originParams: Array<String>) = Unit
+    override fun onFunction(userId: Int, chatId: Long, message: TdApi.Message, function: String, param: String, params: Array<String>, originParams: Array<String>) = Unit
 
     open fun onFunction() = Unit
 
