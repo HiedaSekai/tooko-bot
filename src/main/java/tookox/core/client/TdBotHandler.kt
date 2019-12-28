654ea932d34356509207879c9dce482fefd33653
風間わたる <i@wataru.moe>
2019-12-28 18:13:05 +0800

    _(:з」∠)_

--- src/main/java/tookox/core/client/TdBotHandler.kt
+++ src/main/java/tookox/core/client/TdBotHandler.kt
@@ -1,13 +1,11 @@
 package tookox.core.client
 
 import tooko.td.TdApi
-import tooko.td.client.TdException
 
 open class TdBotHandler : TdHandler(), TdBotAbsHandler {
 
     override val client: TdBot get() = super.client as TdBot
 
-    override fun onLoginFailed(ex: TdException) = Unit
     override fun onUndefinedFunction(userId: Int, chatId: Long, message: TdApi.Message, function: String, param: String, params: Array<String>, originParams: Array<String>) = Unit
     override fun onFunction(userId: Int, chatId: Long, message: TdApi.Message, function: String, param: String, params: Array<String>, originParams: Array<String>) = Unit
 
