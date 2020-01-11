2ca9f3584a4f77dbaeeedb86af1571f02a84cc9b
風間わたる <i@wataru.moe>
2020-01-11 17:05:44 +0800

    _(:з」∠)_

--- src/main/java/tookox/vote/VoteUpdateTask.kt
+++ src/main/java/tookox/vote/VoteUpdateTask.kt
@@ -8,9 +8,57 @@ import java.util.*
 
 object VoteUpdateTask : TimerTask() {
 
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val html = HttpUtil.createGet("https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html")
+                .disableCache()
+                .execute()
+                .body()
+
+        val pfp = html.substringAfter("宋楚瑜")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        val pfpPer = html.substringAfter("宋楚瑜")
+                .substringAfter("tdAlignRight")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        val tnp = html.substringAfter("韓國瑜")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        val tnpPer = html.substringAfter("韓國瑜")
+                .substringAfter("tdAlignRight")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        val dpp = html.substringAfter("蔡英文")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        val dppPer = html.substringAfter("蔡英文")
+                .substringAfter("tdAlignRight")
+                .substringAfter("tdAlignRight\">")
+                .substringBefore("<")
+
+        println("""
+                
+                宋楚瑜 (親民黨): 得票 {}(万) 占比 {}%
+                韓國瑜 (中國國民黨): 得票 {}(万) 占比 {}%
+                蔡英文 (民主進步黨): 得票 {}(万) 占比 {}%
+                
+            """.trimIndent().input(pfp,pfpPer,tnp,tnpPer,dpp,dppPer))
+
+    }
+
     override fun run() {
 
-        val html = HttpUtil.createGet("https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html").disableCache().execute().body()
+        val html = HttpUtil.createGet("https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html")
+                .disableCache()
+                .execute()
+                .body()
 
         val pfp = html.substringAfter("宋楚瑜")
                 .substringAfter("tdAlignRight\">")
