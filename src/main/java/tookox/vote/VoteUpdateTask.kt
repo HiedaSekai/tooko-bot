7e06bca154bb6469cfed25c31c5c58afefd2f76a
風間わたる <i@wataru.moe>
2020-01-11 16:54:32 +0800

    _(:з」∠)_

--- src/main/java/tookox/vote/VoteUpdateTask.kt
+++ src/main/java/tookox/vote/VoteUpdateTask.kt
@@ -10,7 +10,7 @@ object VoteUpdateTask : TimerTask() {
 
     override fun run() {
 
-        val html = HttpUtil.get("https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html")
+        val html = HttpUtil.createGet("https://www.cec.gov.tw/pc/zh_TW/P1/n00000000000000000.html").disableCache().execute().body()
 
         val pfp = html.substringAfter("宋楚瑜")
                 .substringAfter("tdAlignRight\">")
@@ -43,9 +43,9 @@ object VoteUpdateTask : TimerTask() {
 
             inputText = """
                 
-                宋楚瑜 (親民黨): 得票 {} 占比 {}%
-                韓國瑜 (中國國民黨): 得票 {} 占比 {}%
-                蔡英文 (民主進步黨): 得票 {} 占比 {}%
+                宋楚瑜 (親民黨): 得票 {}(万) 占比 {}%
+                韓國瑜 (中國國民黨): 得票 {}(万) 占比 {}%
+                蔡英文 (民主進步黨): 得票 {}(万) 占比 {}%
                 
             """.trimIndent().input(pfp,pfpPer,tnp,tnpPer,dpp,dppPer)
 
