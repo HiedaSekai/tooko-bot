ea1ec3c0ad1ca2c83dbbf033c41c4942c70f4576
風間わたる <i@wataru.moe>
2020-01-11 20:04:23 +0800

    _(:з」)_

--- src/main/java/tookox/core/env/Env.java
+++ src/main/java/tookox/core/env/Env.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2019 - 2020 KazamaWataru
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tookox.core.env;
 
 import cn.hutool.core.util.*;
@@ -25,7 +41,7 @@ public class Env {
     public static String SERVICE;
     public static String TWITTER_BOT_TOKEN;
     public static boolean TWITTER_PUBLIC;
-  //  public static ApiToken[] TWITTER_API_TOKENS;
+    //  public static ApiToken[] TWITTER_API_TOKENS;
     public static int DEVELOPER = 896711046;
 
     public static String NSFW_SERVER;
