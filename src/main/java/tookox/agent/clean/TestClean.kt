bdeb04852e57c2150f1b740e75c499e248f69605
風間わたる <i@wataru.moe>
2020-01-15 12:04:05 +0800

    _(:з」∠)_

--- src/main/java/tookox/agent/clean/TestClean.kt
+++ src/main/java/tookox/agent/clean/TestClean.kt
@@ -45,7 +45,7 @@ class TestClean : TdBotHandler() {
 
             val dcId = param.toInt()
 
-            val pool = Executors.newFixedThreadPool(5)
+            val pool = Executors.newFixedThreadPool(10)
 
             val deferredes = LinkedList<Deferred<Unit>>()
 
