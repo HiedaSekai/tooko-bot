87d0cdab9721f57ac5fd6dbcc3686b9a0a1166e9
風間わたる <i@wataru.moe>
2020-01-15 10:00:20 +0800

    _(:з」∠)_

--- src/main/java/tookox/agent/clean/TestClean.kt
+++ src/main/java/tookox/agent/clean/TestClean.kt
@@ -57,7 +57,7 @@ class TestClean : TdBotHandler() {
 
                         val client = CleanClient(dcId, index)
 
-                        client.start()
+                        client.start(false)
 
                         while (!client.closed) delay(100L)
 
